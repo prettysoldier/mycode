507b50d213341f9c15852a549262129d4c7e7054
phenix <louye666@qq.com>
2018-11-14 10:31:55 +0800

    fix

--- src/test/java/map/HashMapTest.java
+++ src/test/java/map/HashMapTest.java
@@ -1,11 +1,11 @@
-package test.java;
+package test.java.map;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class HashMapTest {
     public static void main(String[] args) {
-        Map map  = new HashMap(12);
+        Map<String, String> map = new HashMap<String, String>(1, 1);
         System.out.println(map);
         map.put("k", "v");
     }
