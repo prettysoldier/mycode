12ee2b6919f4892d7e4a223af34c5f32e31c38b0
phenix <louye666@qq.com>
2018-11-27 14:48:13 +0800

    fix

--- src/test/java/overload/Overload.java
+++ src/test/java/overload/Overload.java
@@ -1,16 +1,33 @@
 package test.java.overload;
 
+import java.io.Serializable;
+
 /**
  * 重载优先级：自动转型和自动装箱的顺序
  * 顺序：char->int-> long ->float-> double-> 自动装箱-> 接口或父类-> Object -> char...
+ * byte->short->int-> long ->float-> double-> 自动装箱-> 接口或父类-> Object ->
+ * boolean->Boolean->Serializable/Comparable->Object->boolean.../Boolean...
  *
  * @author Shuaijun He
  */
 public class Overload {
 
-//    public static void sayHello(char arg) {
-//        System.out.println("hello char : " + arg);
+//    public static void sayHello(boolean arg) {
+//        System.out.println("hello boolean : " + arg);
 //    }
+
+    public static void sayHello(char arg) {
+        System.out.println("hello char : " + arg);
+    }
+
+    public static void sayHello(byte arg) {
+        System.out.println("hello byte : " + arg);
+    }
+
+    public static void sayHello(short arg) {
+        System.out.println("hello short : " + arg);
+    }
+
 
     public static void sayHello(int arg) {
         System.out.println("hello int : " + arg);
@@ -28,45 +45,62 @@ public class Overload {
         System.out.println("hello double : " + arg);
     }
 
-    public static void sayHello(byte arg) {
-        System.out.println("hello byte : " + arg);
-    }
 
-    public static void sayHello(short arg) {
-        System.out.println("hello short : " + arg);
-    }
-
+//    public static void sayHello(Boolean arg) {
+//        System.out.println("hello Boolean : " + arg);
+//    }
+//    public static void sayHello(Byte arg) {
+//        System.out.println("hello Byte : " + arg);
+//    }
+//
 //    public static void sayHello(Character arg) {
 //        System.out.println("hello character : " + arg);
 //    }
 
-//    public static void sayHello(Serializable arg) {
-//        System.out.println("hello Serializable : " + arg);
+
+
+    public static void sayHello(Serializable arg) {
+        System.out.println("hello Serializable : " + arg);
+    }
+
+    public static void sayHello(Comparable<?> arg) {
+        System.out.println("hello Comparable : " + arg);
+    }
+
+    public static void sayHello(Object arg) {
+        System.out.println("hello Object : " + arg);
+    }
+
+//    public static void sayHello(boolean... arg) {
+//        System.out.println("hello boolean... : " + arg[0]);
 //    }
 
-//    public static void sayHello(Comparable<?> arg) {
-//        System.out.println("hello Comparable : " + arg);
+//    public static void sayHello(Boolean... arg) {
+//        System.out.println("hello Boolean... : " + arg[0]);
 //    }
 
 //    public static void sayHello(char... arg) {
-//        System.out.println("hello char : " + arg);
+//        System.out.println("hello char : " + arg[0]);
 //    }
-
+//
+//    public static void sayHello(short... arg) {
+//        System.out.println("hello short... : " + arg[0]);
+//    }
+//
 //    public static void sayHello(int... arg) {
-//        System.out.println("hello int... : " + arg);
+//        System.out.println("hello int... : " + arg[0]);
 //    }
-
+//
 //    public static void sayHello(float... arg) {
-//        System.out.println("hello float... : " + arg);
+//        System.out.println("hello float... : " + arg[0]);
 //    }
 
-//    public static void sayHello(Object arg) {
-//        System.out.println("hello object : " + arg);
-//    }
+
 
     public static void main(String[] args) {
 
-        Overload.sayHello('a');
+//        Overload.sayHello("a".getBytes()[0]);
+//        Overload.sayHello(true);
 //        Overload.sayHello(null);
     }
 
