f215c8984d33c195d5ed28a6a360208148d7e4d8
prettysoldier <louye666@qq.com>
2019-11-18 16:22:42 +0800

    状态模式

---
+++ src/main/java/pattern/state/AbstractLiftState.java
@@ -0,0 +1,131 @@
+package pattern.state;
+
+/**
+ * @Desc 抽象状态(State)角色：定义一个接口，用以封装环境（Context）对象的一个特定的状态所对应的行为。
+ * @Author shuaijunhe
+ * @CreateTime 2018/11/14 20:05
+ **/
+public abstract class AbstractLiftState {
+
+    protected LiftContext lift;
+
+    public LiftContext getLift() {
+        return lift;
+    }
+
+    public void setLift(LiftContext lift) {
+        this.lift = lift;
+    }
+
+
+    public abstract void open();
+    public abstract void close();
+    public abstract void run();
+    public abstract void stop();
+
+
+}
+
+/**
+ * 具体状态(ConcreteState)角色：每一个具体状态类都实现了环境（Context）的一个状态所对应的行为。
+ */
+class StopState extends AbstractLiftState {
+
+    @Override
+    public void open() {
+        this.getLift().setLiftState(LiftContext.openState);
+        this.getLift().getLiftState().open();
+    }
+
+    @Override
+    public void close() {
+        System.out.println("本来就关着呢~~");
+    }
+
+    @Override
+    public void run() {
+
+        this.getLift().setLiftState(LiftContext.runState);
+        this.getLift().getLiftState().run();
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("停止...");
+    }
+}
+
+class OpenState extends AbstractLiftState {
+
+
+    @Override
+    public void open() {
+        System.out.println("开门...");
+    }
+
+    @Override
+    public void close() {
+        this.getLift().setLiftState(LiftContext.closeState);
+        this.getLift().getLiftState().close();
+    }
+
+    @Override
+    public void run() {
+        System.out.println("开着电梯门运行？！");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("本来也没走啊~");
+    }
+}
+
+class RunState extends AbstractLiftState {
+
+
+    @Override
+    public void open() {
+        System.out.println("运行时不允许开门，很危险！");
+    }
+
+    @Override
+    public void close() {
+        System.out.println("本来就是关着的~~");
+    }
+
+    @Override
+    public void run() {
+        System.out.println("运行...");
+    }
+
+    @Override
+    public void stop() {
+        this.getLift().setLiftState(LiftContext.stopState);
+        this.getLift().getLiftState().stop();
+    }
+}
+
+class CloseState extends AbstractLiftState {
+
+    @Override
+    public void open() {
+        this.getLift().setLiftState(LiftContext.openState);
+        this.getLift().getLiftState().open();
+    }
+
+    @Override
+    public void close() {
+        System.out.println("关门...");
+    }
+
+    @Override
+    public void run() {
+        this.getLift().setLiftState(LiftContext.runState);
+        this.getLift().getLiftState().run();
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("本来就是停着的~~");
+    }
+}
--- src/main/java/pattern/state/LiftContext.java
+++ src/main/java/pattern/state/LiftContext.java
@@ -1,13 +1,15 @@
 package pattern.state;
 
 /**
- * @Desc
+ * @Desc 环境(Context)角色，也称上下文：定义客户端所感兴趣的接口，并且保留一个具体状态类的实例。
+ * 这个具体状态类的实例给出此环境对象的现有状态。
+ *
  * @Author shuaijunhe
  * @CreateTime 2018/11/14 20:09
  **/
-public class Lift extends LiftState {
+public class LiftContext extends AbstractLiftState {
 
-    private LiftState liftState;
+    private AbstractLiftState liftState;
 
     static CloseState closeState = new CloseState();
     static OpenState openState = new OpenState();
@@ -16,11 +18,11 @@ public class Lift extends LiftState {
 
 
 
-    public LiftState getLiftState() {
+    public AbstractLiftState getLiftState() {
         return liftState;
     }
 
-    public void setLiftState(LiftState liftState) {
+    public void setLiftState(AbstractLiftState liftState) {
         this.liftState = liftState;
         this.liftState.setLift(this);
     }
