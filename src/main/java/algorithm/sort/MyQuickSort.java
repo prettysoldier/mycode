5841713863431e900e771ce31444406d5f91cea1
prettysoldier <louye666@qq.com>
2019-12-27 17:47:03 +0800

    快排的三分单向扫描、三分双向扫描、双轴快排

--- src/main/java/algorithm/sort/MyQuickSort.java
+++ src/main/java/algorithm/sort/MyQuickSort.java
@@ -1,12 +1,13 @@
 package algorithm.sort;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
 /**
  * 快速排序 最坏：O(N^2) 平均：O(NlogN) 取决于枢纽元的选取（三数中值分割法）
- * 
+ * https://blog.csdn.net/Holmofy/article/details/71168530
  * @author Shuaijun He
  */
 public class MyQuickSort extends MySortBase {
@@ -16,7 +17,11 @@ public class MyQuickSort extends MySortBase {
      */
     @Override
     protected <T extends Comparable<? super T>> void sort(T[] a) {
-        MyQuickSort.quicksort(a, 0, a.length - 1);
+//        quicksort(a, 0, a.length - 1);
+//        div3ScanSort(a, 0, a.length - 1);
+//        div3DualScanSort(a, 0, a.length - 1);
+//        dualPivotQuickSort(a, 0, a.length - 1);
+        Arrays.sort(a);
     }
 
     private static final int CUTOFF = 10;
@@ -85,6 +90,110 @@ public class MyQuickSort extends MySortBase {
         a[r] = tmp;
     }
 
+    private static <T extends Comparable<? super T>> void div3ScanSort(T[] items,
+                                                                    int start, int end) {
+        if (start < end) {
+            T pivot = items[start];
+            int i = start, j = end, k = start + 1;
+            while (k <= j) {
+                if (items[k].compareTo(pivot) < 0) {
+                    swap(items, i, k);
+                    i++;
+                    k++;
+                } else if (items[k].compareTo(pivot) > 0) {
+                    swap(items, j, k);
+                    j--;
+                } else {
+                    k++;
+                }
+            }
+            div3ScanSort(items, start, i - 1);
+            div3ScanSort(items, j + 1, end);
+        }
+
+    }
+
+    private static <T extends Comparable<? super T>> void div3DualScanSort(T[] items, int start, int end) {
+        if (start < end) {
+            T pivot = items[start];
+            int i = start, j = end, k = start + 1;
+
+            OUT_LOOP:
+            while (k <= j) {
+                if (items[k].compareTo(pivot) < 0) {
+                    swap(items, i++, k++);
+                } else if (items[k] == pivot) {
+                    k++;
+                } else {
+                    // j向左扫描，直到一个不大于pivot的元素
+                    while (items[j].compareTo(pivot) > 0) {
+                        j--;
+                        if (k > j) {
+                            // 后面的待排元素全大于pivot，直接结束排序
+                            break OUT_LOOP;
+                        }
+                    }
+                    if (items[j].compareTo(pivot) < 0) {
+                        swap(items, j, k);
+                        swap(items, i, k);
+                        i++;
+                    } else {
+                        swap(items, j, k);
+                    }
+                    k++;
+                    j--;
+                }
+            }
+            div3DualScanSort(items, start, i - 1);
+            div3DualScanSort(items, j + 1, end);
+        }
+    }
+
+
+    private static <T extends Comparable<? super T>> void dualPivotQuickSort(T[] items, int start, int end) {
+        if (start < end) {
+            if (items[start].compareTo(items[end]) > 0) {
+                swap(items, start, end);
+            }
+            T pivot1 = items[start];
+            T pivot2 = items[end];
+
+            int i = start, j = end, k = start + 1;
+
+            BREAK_LOOP:
+            while (k <= j) {
+                if (items[k].compareTo(pivot1) < 0) {
+                    swap(items, i++, k++);
+                } else if (items[k].compareTo(pivot2) <= 0) {
+                    k++;
+                } else {
+                    while (items[j].compareTo(pivot2) > 0) {
+                        j--;
+                        if (j < k) {
+                            // 扫描终止
+                            break BREAK_LOOP;
+                        }
+                    }
+
+                    if (items[j].compareTo(pivot1) < 0) {
+                        swap(items, j, k);
+                        swap(items, i++, k);
+                    } else {
+                        swap(items, j, k);
+                    }
+                    k++;
+                    j--;
+                }
+            }
+            if(j - i < end - start){
+
+                dualPivotQuickSort(items, start, i - 1);
+                dualPivotQuickSort(items, i, j);
+                dualPivotQuickSort(items, j + 1, end);
+            }
+        }
+    }
+
     /**
      * 实现1：用list实现
      */
@@ -115,7 +224,7 @@ public class MyQuickSort extends MySortBase {
         }
     }
 
-    private static final int M = 1_000_0000;
+    private static final int M = 100_0000;
 
     public static void main(String[] args) {
         Random r = new Random();
@@ -124,5 +233,8 @@ public class MyQuickSort extends MySortBase {
             a[i] = r.nextInt(MyQuickSort.M);
         }
         new MyQuickSort().sortWithLog(a);
+//        a = new Integer[]{3, 2, 1, 2, 6, 3, 5, 2, 7, 4};
+//        new MyQuickSort().sortWithLog(a);
+//        System.out.println(Arrays.toString(a));
     }
 }
