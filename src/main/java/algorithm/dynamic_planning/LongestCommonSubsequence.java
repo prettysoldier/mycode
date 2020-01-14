d3fd45e56dd7740be059b6770d121c0bb0dbcce7
prettysoldier <louye666@qq.com>
2020-01-14 14:42:11 +0800

    fix

--- src/main/java/algorithm/dynamic_planning/LongestCommonSubsequence.java
+++ src/main/java/algorithm/dynamic_planning/LongestCommonSubsequence.java
@@ -44,9 +44,10 @@ public class LongestCommonSubsequence {
         for(int i = 1; i <= A.length(); i++){
             for(int j = 1; j <= A.length(); j++){
 
-                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 if(A.charAt(i - 1) == B.charAt(j - 1)) {
                     dp[i][j] = dp[i - 1][j - 1] + 1;
+                }else{
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
         }
