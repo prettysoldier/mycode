23f929f6990d68dddf482827024503728ea32cdf
Phenix <louye666@qq.com>
2020-01-17 01:38:31 +0800

    fix

---
+++ src/main/java/source_code_java/java_/syntax/switch_/MySwitch.java
@@ -0,0 +1,43 @@
+package source_code_java.java_.syntax.switch_;
+
+import source_code_java.java_.syntax.enum_.MyEnum;
+
+/**
+ * @author heshuaijun
+ * @date 2020/1/17 0:58
+ */
+public class MySwitch {
+
+    public static void main (String[] args) {
+
+        String s = "asd";
+        switch (s){
+            case "qqq" :
+                System.out.println("qqq");
+                break;
+            case "as" :
+                System.out.println("as");
+                break;
+            case "asd" :
+                System.out.println("asd");
+                break;
+        }
+
+
+    }
+
+    public static void f () {
+
+        MyEnum v = MyEnum.ONE;
+        switch (v){
+            case ONE:
+                System.out.println("ONE");
+                break;
+            case SECOND :
+                System.out.println("SECOND");
+                break;
+        }
+
+
+    }
+}
