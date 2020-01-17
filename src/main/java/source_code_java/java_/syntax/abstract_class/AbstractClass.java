f9f5d9859670fc02795f5212b1f9281288af8e0a
prettysoldier <louye666@qq.com>
2020-01-17 11:52:22 +0800

    fix

---
+++ src/main/java/source_code_java/java_/syntax/abstract_class/AbstractClass.java
@@ -0,0 +1,18 @@
+package source_code_java.java_.syntax.abstract_class;
+
+/**
+ * 抽象方法可以存在构造器
+ * @author hsj
+ * @create 2020/1/17
+ */
+public abstract class AbstractClass {
+
+    private int i;
+
+    public AbstractClass(int i) {
+        this.i = i;
+    }
+
+    public static void main(String[] args) {
+    }
+}
